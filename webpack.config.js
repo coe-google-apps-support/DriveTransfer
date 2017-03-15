6a23e19aea0197decd5f7450a3466fd9461749f6
Jared Rewerts <jared.rewerts@edmonton.ca>
2017-03-15 11:15:39 -0600

    Switched the webpack config around. Might switch to using a seperate build hierarchy later.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'build');
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
 
 var config = {
