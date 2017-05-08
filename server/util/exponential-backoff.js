bf7f7d2769e4c2b81dafe96ece257129be4f2c7e
j-rewerts <rewerts@ualberta.ca>
2017-05-08 12:10:27 -0600

    Built an exponential backoff function for use with the Google API throughput issues.

---
+++ server/util/exponential-backoff.js
@@ -0,0 +1,28 @@
+const sleep = require('./sleep.js');
+
+function exponentialBackoff(toTry, max, delay) {
+  console.log('max', max, 'next delay', delay);
+
+  return new Promise((resolve, reject) => {
+    try {
+      toTry();
+      resolve();
+    }
+    catch (err) {
+      if (max > 0) {
+        setTimeout(function() {
+          exponentialBackoff(toTry, --max, delay * 2, callback).then(() => {
+            resolve();
+          });
+        }, delay);
+
+      } else {
+        reject(new Error('This is reeeeally screwed.'));
+      }
+    }
+  })
+
+
+}
+
+module.exports = exponentialBackoff;
