771a2666bc3a93bb9349edab38df5ae9c930005a
j-rewerts <rewerts@ualberta.ca>
2017-05-08 13:19:45 -0600

    Cleaned up exponentialBackoff.

--- server/util/exponential-backoff.js
+++ server/util/exponential-backoff.js
@@ -1,6 +1,12 @@
+/**
+ * Attempts to try a function repeatedly until either success or max number of tries has been reached.
+ *
+ * @param {Function} toTry The function to try repeatedly. Thrown errors cause a retry.
+ * @param {number} max The maximum number of function call attempts.
+ * @param {number} delay The base delay in ms. This doubles after every attempt.
+ * @return {Promise} Rejects when toTry has failed max times. Resolves if successful once.
+ */
 function exponentialBackoff(toTry, max, delay) {
-  console.log('max', max, 'next delay', delay);
-
   return new Promise((resolve, reject) => {
     try {
       toTry();
@@ -9,18 +15,16 @@ function exponentialBackoff(toTry, max, delay) {
     catch (err) {
       if (max > 0) {
         setTimeout(function() {
-          exponentialBackoff(toTry, --max, delay * 2, callback).then(() => {
+          exponentialBackoff(toTry, --max, delay * 2).then(() => {
             resolve();
           });
         }, delay);
 
       } else {
-        reject(new Error('This is reeeeally screwed.'));
+        reject(err);
       }
     }
-  })
-
-
+  });
 }
 
 module.exports = exponentialBackoff;
