ee06580ff850da332bdb4a756e3ba6c37df015d2
Jared Rewerts <jared.rewerts@edmonton.ca>
2017-05-10 15:32:28 -0600

    Cleaned up the only view controller.

--- server/view/main.js
+++ server/view/main.js
@@ -1,9 +1,5 @@
 const path = require('path');
 
 exports.view = function(req, res, next) {
-  res.sendFile(path.resolve(__dirname, '..', '..', 'client', 'src', 'static', 'index.ejs'));
-
-  // res.status(200).json({
-  //   message: "GRAWK"
-  // })
+  res.sendFile(path.resolve(__dirname, '..', '..', 'client', 'src', 'static', 'index.html'));
 }
