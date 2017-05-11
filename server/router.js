b0addea5c47082534afc6c10a84e6b5e7e587d21
Jared Rewerts <jared.rewerts@edmonton.ca>
2017-05-11 17:17:10 -0600

    Added CORS and forced auth.

--- server/router.js
+++ server/router.js
@@ -7,6 +7,7 @@ const Redirect = require('./controller/redirect.js');
 const Task = require('./model/task.js');
 const Session = require('express-session');
 const logger = require('morgan');
+const cors = require('cors');
 
 const sess = {
   secret: 'fasdkh7f4qjhadf6kashfr347ajpv',
@@ -15,20 +16,24 @@ const sess = {
 }
 
 module.exports = function(app) {
-  app.use(express.static(path.resolve(__dirname, '..', 'client', 'src', 'static')));
   app.use(Session(sess));  // Use req.session to get and set user-specific properties.
   app.use(logger('dev'));
+  app.use(cors());
 
-  const apiRoutes = express.Router();
-  apiRoutes.get('/list', authAPI.requireAuth, controller.list);
-  apiRoutes.get('/transfer', authAPI.requireAuth, controller.transfer);
-  apiRoutes.get('/reset', (req) => req.session.destroy);
+  app.get('/api/list', authAPI.requireAuth, controller.list);
+  app.get('/api/transfer', authAPI.requireAuth, controller.transfer);
+  app.get('/api/reset', (req) => req.session.destroy);
 
-  const viewRoutes = express.Router();
-  viewRoutes.get('/', mainView.view);
+  app.get('/view', authAPI.requireAuth, mainView.view);
 
-  app.use('/api', apiRoutes);
-  app.use('/view', viewRoutes);
-  app.use('/redirect', Redirect.redirect);
-  app.use('/auth', authAPI.requireAuth);
+  app.get('/', (req, res) => {
+    console.log('redirect');
+    res.redirect('/view');
+
+  });
+
+  app.get('/redirect', Redirect.redirect);
+  app.get('/auth', authAPI.requireAuth);
+
+  app.use(express.static(path.resolve(__dirname, '..', 'client', 'src', 'static')));
 }
