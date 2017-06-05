880c0709348da54041ca47d14063f6da24904869
j-rewerts <rewerts@ualberta.ca>
2017-06-05 15:27:02 -0600

    Added a stepper setup wizard.

--- client/src/components/main.jsx
+++ client/src/components/main.jsx
@@ -5,6 +5,8 @@ import RaisedButton from 'material-ui/RaisedButton';
 import TransferService from '../services/transfer.js';
 import {grey600} from 'material-ui/styles/colors';
 import State from '../model/state.js'
+import Wizard from './wizard.jsx';
+import styles from './main.css';
 
 const baseDisplay = {
   display: 'flex',
@@ -36,7 +38,8 @@ class Main extends React.Component {
       disabled: false,
       folderID: '',
       newOwner: '',
-      transferService: new TransferService()
+      transferService: new TransferService(),
+      showWizard: false,
     };
   }
 
@@ -57,19 +60,30 @@ class Main extends React.Component {
     State.setState({newOwner: event.target.value})
   }
 
+  startWizard() {
+    this.setState({showWizard: true});
+  }
+
   render () {
+    const {showWizard} = this.state;
+
     return (
       <Paper zDepth={0} style={baseDisplay}>
         <img width='200px' height='200px' src='icon.png'/>
         <h1 style={textStyle}>Drive Transfer</h1>
-        <TextField hintText='New Owner' value={this.state.newOwner} onChange={this.setNewOwner.bind(this)}/>
-        <TextField hintText='Folder ID' value={this.state.folderID} onChange={this.setFolderID.bind(this)}/>
+        {/* <TextField hintText='New Owner' value={this.state.newOwner} onChange={this.setNewOwner.bind(this)}/>
+        <TextField hintText='Folder ID' value={this.state.folderID} onChange={this.setFolderID.bind(this)}/> */}
+
+        <Wizard className={styles.hideable + ' ' + (showWizard ? styles.show : styles.hidden)} />
+
         <RaisedButton
+          className={styles.hideable + ' ' + (showWizard ? styles.hidden : styles.show)}
           disabled={this.state.disabled}
           label='Start'
           primary={true}
           style={buttonStyle}
-          onTouchTap={this.startTransfer.bind(this)}/>
+          onTouchTap={this.startWizard.bind(this)}/>
+
       </Paper>
     );
   }
