1adc6c40f64febb8fc70a1fd7b9489502f1b78ee
Jared Rewerts <jared.rewerts@edmonton.ca>
2017-06-06 10:32:27 -0600

    Altered the wizard to use a button.

--- client/src/components/wizard.jsx
+++ client/src/components/wizard.jsx
@@ -1,25 +1,62 @@
 import React from 'react';
-import {
-  Step,
-  Stepper,
-  StepLabel,
-} from 'material-ui/Stepper';
 import RaisedButton from 'material-ui/RaisedButton';
 import FlatButton from 'material-ui/FlatButton';
+import TextField from 'material-ui/TextField';
+import FontIcon from 'material-ui/FontIcon';
+import Paper from 'material-ui/Paper';
+
+import State from '../model/state.js';
 
 const styles = {
   root: {
     width: '100%',
-    maxWidth: 700,
+    maxWidth: '400px',
+    height: '200px',
+    display: '-webkit-box',
+    display: '-moz-box',
+    display: '-ms-flexbox',
+    display: '-webkit-flex',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  icon: {
+    fontSize: '48px',
+    color: 'rgb(66, 66, 66)',
+    margin: '12px',
   },
   content: {
     margin: '0 16px',
+    display: '-webkit-box',
+    display: '-moz-box',
+    display: '-ms-flexbox',
+    display: '-webkit-flex',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
   },
   actions: {
-    marginTop: 12,
+    marginTop: '12px',
+    display: '-webkit-box',
+    display: '-moz-box',
+    display: '-ms-flexbox',
+    display: '-webkit-flex',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  folderPicker: {
+    display: '-webkit-box',
+    display: '-moz-box',
+    display: '-ms-flexbox',
+    display: '-webkit-flex',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: '12px',
+    cursor: 'pointer',
   },
   backButton: {
-    marginRight: 12,
+    marginRight: '12px',
   },
 };
 
@@ -32,18 +69,28 @@ class Wizard extends React.Component {
     this.handlePrev = this.handlePrev.bind(this);
 
     this.state = {
-      finished: false,
       stepIndex: 0,
+      stepDisabled: false,
+      finished: false,
+      newOwner: '',
+      folder: '',
     };
   };
 
   handleNext() {
     const {stepIndex} = this.state;
 
-    this.setState({
-      stepIndex: stepIndex + 1,
-      finished: stepIndex >= 2,
-    });
+    if (stepIndex + 1 >= 2) {
+      this.setState({
+        stepDisabled: true,
+        finished: true,
+      });
+    }
+    else {
+      this.setState({
+        stepIndex: stepIndex + 1,
+      });
+    }
   };
 
   handlePrev() {
@@ -56,55 +103,64 @@ class Wizard extends React.Component {
     }
   };
 
+  setNewOwner(event) {
+    State.setState({newOwner: event.target.value});
+  }
+
+  selectFolder() {
+    console.log('OPENING FOLDER PICKER');
+  }
+
   getStepContent(stepIndex) {
     switch (stepIndex) {
       case 0:
-      return 'Select campaign settings...';
+      return (
+        <TextField hintText='New Owner' />
+      );
 
       case 1:
-      return 'What is an ad group anyways?';
-
-      case 2:
-      return 'This is the bit I really care about!';
+      return (
+        <FlatButton style={{
+          height: '100px',
+          width: '300px',
+        }} onClick={this.selectFolder.bind(this)}>
+          <FontIcon style={styles.icon} className="material-icons">folder</FontIcon>
+          <div>Select folder</div>
+        </FlatButton>
+      );
 
       default:
-      return 'You\'re a long way from home sonny jim!';
+      return (
+        <Paper style={styles.folderPicker} zDepth={1} onClick={this.selectFolder.bind(this)}>
+          <FontIcon style={styles.icon} className="material-icons">folder</FontIcon>
+          <div>Select folder</div>
+        </Paper>
+      );
     }
   }
 
   render() {
-    const {finished, stepIndex} = this.state;
-    const contentStyle = {margin: '0 16px'};
+    const {stepIndex, stepDisabled} = this.state;
 
     return (
       <div style={styles.root} className={this.props.className}>
-        <Stepper activeStep={stepIndex}>
-          <Step>
-            <StepLabel>Select campaign settings</StepLabel>
-          </Step>
-          <Step>
-            <StepLabel>Create an ad group</StepLabel>
-          </Step>
-          <Step>
-            <StepLabel>Create an ad</StepLabel>
-          </Step>
-        </Stepper>
-        <div style={contentStyle}>
-          <div>
-            <p>{this.getStepContent(stepIndex)}</p>
-            <div style={{marginTop: 12}}>
-              <FlatButton
-                label="Back"
-                disabled={stepIndex === 0}
-                onTouchTap={this.handlePrev}
-                style={{marginRight: 12}}
-              />
-              <RaisedButton
-                label={stepIndex === 2 ? 'Finish' : 'Next'}
-                primary={true}
-                onTouchTap={this.handleNext}
-              />
-            </div>
+        <div style={styles.content}>
+          {this.getStepContent(stepIndex)}
+        </div>
+        <div >
+          <div style={styles.actions}>
+            <FlatButton
+              label="Back"
+              disabled={stepIndex === 0 || stepDisabled}
+              onTouchTap={this.handlePrev}
+              style={{marginRight: 12}}
+            />
+            <RaisedButton
+              disabled={stepDisabled}
+              label={stepIndex === 1 ? 'Start' : 'Next'}
+              primary={true}
+              onTouchTap={this.handleNext}
+            />
           </div>
         </div>
       </div>
