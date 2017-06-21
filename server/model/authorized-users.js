df3b7c57ce51b2c4a96965a5d1b94e3f976b698c
Jared Rewerts <jared.rewerts@edmonton.ca>
2017-06-21 16:06:45 -0600

    Added a mongodb client creation function.

--- server/model/authorized-users.js
+++ server/model/authorized-users.js
@@ -1,8 +1,21 @@
+const G = './global.js';
+
 class AuthorizedUsers {
 
   constructor() {
     console.log('Building user base.');
     this.users = {};
+  }
+
+  reloadUsers() {
+    let collectionl
+    G.getMongoClient().then((db) => {
+      collection = db.collection('users');
+
+      return collection.find({}).toArray();
+    }).then((result) => {
+      console.log(result);
+    });
   }
 
   getUser(key) {
