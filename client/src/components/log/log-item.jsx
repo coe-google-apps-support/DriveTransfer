0fc938db788ee71f390e4b5196ebe5265006cff3
Jared Rewerts <jared.rewerts@edmonton.ca>
2017-06-22 16:37:20 -0600

    Cleaned up LogItems.

--- client/src/components/log/log-item.jsx
+++ client/src/components/log/log-item.jsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {ListItem} from 'material-ui/List';
-import Avatar from 'material-ui/Avatar';
-import {blue500, yellow600} from 'material-ui/styles/colors';
 import MIMEInfo from '../../services/mime-info.jsx';
 import Moment from 'moment';
-
-// Icons
-import EditorInsertChart from 'material-ui/svg-icons/editor/insert-chart';
-import ActionAssignment from 'material-ui/svg-icons/action/assignment';
-import ActionInfo from 'material-ui/svg-icons/action/info';
-import FileFolder from 'material-ui/svg-icons/file/folder';
 
 const maxLength = 26;
 
@@ -24,14 +16,6 @@ class LogItem extends React.Component {
   }
 
   formatDate(dateString) {
-    const months = [
-      'January', 'February', 'March',
-      'April', 'May', 'June',
-      'July', 'August', 'September',
-      'October', 'November', 'December'
-    ];
-    let date = new Date(dateString);
-
     return Moment(dateString).format('MMMM Do, YYYY');
   }
 
