8709a3a4b7faac05a319b5f43aae6c0aba9fb1aa
Jared Rewerts <jared.rewerts@edmonton.ca>
2017-06-22 16:25:24 -0600

    Fixed the displayed date on log items.

--- client/src/components/log/log-item.jsx
+++ client/src/components/log/log-item.jsx
@@ -4,6 +4,7 @@ import {ListItem} from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
 import {blue500, yellow600} from 'material-ui/styles/colors';
 import MIMEInfo from '../../services/mime-info.jsx';
+import Moment from 'moment';
 
 // Icons
 import EditorInsertChart from 'material-ui/svg-icons/editor/insert-chart';
@@ -22,6 +23,18 @@ class LogItem extends React.Component {
     window.open(this.props.file.webViewLink);
   }
 
+  formatDate(dateString) {
+    const months = [
+      'January', 'February', 'March',
+      'April', 'May', 'June',
+      'July', 'August', 'September',
+      'October', 'November', 'December'
+    ];
+    let date = new Date(dateString);
+
+    return Moment(dateString).format('MMMM Do, YYYY');
+  }
+
   render() {
     let icon = MIMEInfo.getIcon(this.props.file.mimeType);
 
@@ -30,7 +43,7 @@ class LogItem extends React.Component {
         leftAvatar={icon}
         rightIcon={<ActionInfo />}
         primaryText={this.props.file.name.substring(0, maxLength) + '...'}
-        secondaryText="Jan 17, 2014"
+        secondaryText={this.formatDate(this.props.file.createdTime)}
         onTouchTap={this.openURL.bind(this)}
       />
     );
