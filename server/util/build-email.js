e7b0576ab82da9fecc71a7ec0182328c079effaf
Jared Rewerts <jared.rewerts@edmonton.ca>
2017-07-10 17:08:58 -0600

    Added more variables to the email. It now closely resembles the transfer creation API call.

--- server/util/build-email.js
+++ server/util/build-email.js
@@ -1,11 +1,13 @@
 const EJS = require('ejs');
 const readFile = require('./promisey-read-file.js').readFile;
 
-exports.buildRequestEmail = function(recipient, initiator) {
+exports.buildRequestEmail = function(recipient, folderID, initiator, taskID) {
   return readFile('./resources/request-email.txt').then((result) => {
     return EJS.render(result, {
       recipient: recipient,
-      initiator: initiator
+      folderID: folderID,
+      initiator: initiator,
+      taskID: taskID
     });
   });
 }
