c5edfc0ccf7d2d5bda82bdce90e936a8ecf4af2a
Jared Rewerts <jared.rewerts@edmonton.ca>
2017-07-14 11:44:13 -0600

    Added a test runner for Object.filterByKey.

--- server/util/object-filter.js
+++ server/util/object-filter.js
@@ -3,6 +3,7 @@
 * https://stackoverflow.com/questions/5072136/javascript-filter-for-objects
 * @param {Object} obj The Object to filter.
 * @param {Function} predicate The function to use to filter. This function receives a key as a param.
+* Similar to Array.prototype.filter, this function must return a boolean.
 * @return {Object} The newly filtered Object.
 */
 Object.filterByKey = function(obj, predicate) {
