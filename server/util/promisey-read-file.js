a1c3c6b16a16ff56a678d09a6f38a110ba7f6110
Jared Rewerts <jared.rewerts@edmonton.ca>
2017-07-14 13:47:08 -0600

    Added tests for promisey-read-file.

--- server/util/promisey-read-file.js
+++ server/util/promisey-read-file.js
@@ -1,5 +1,10 @@
 var fs = require('fs');
 
+/**
+ * Wraps node.js readFile in a Promise interface. Reads utf-8.
+ * @param {string} fileName The path to the file.
+ * @return {Promise} A Promise that is resolved with value of the file.
+ */
 exports.readFile = function(fileName) {
   return new Promise(function(resolve, reject) {
     fs.readFile(fileName, 'utf-8', function(err, data) {
