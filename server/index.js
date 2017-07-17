edc91e6043cdf95297cf7787d87c93a9992132b4
j-rewerts <rewerts@ualberta.ca>
2017-07-17 14:06:09 -0600

    Added ws as the websockets library.

--- server/index.js
+++ server/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const app = express();
 const router = require('./router.js');
 
+const http = require('http');
+const url = require('url');
+const WebSocket = require('ws');
+
 require('./util/map-to-json.js');
 require('./util/object-filter.js');
 
-var port = 3000;
+let port = 3000;
 
 // This grabs all unhandled Promise rejections and logs them. Otherwise, you get no stacktrace.
 // http://2ality.com/2016/04/unhandled-rejections.html
@@ -15,5 +19,22 @@ process.on('unhandledRejection', (reason) => {
 
 router(app);
 
-app.listen(port);
+// ws
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', function connection(ws, req) {
+  const location = url.parse(req.url, true);
+  console.log('User connected');
+  // You might use location.query.access_token to authenticate or share sessions
+  // or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
+  ws.send('something');
+});
+
+server.listen(port);
 console.log('Your server is running on port ' + port + '.');
