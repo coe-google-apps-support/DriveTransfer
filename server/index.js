a57a93cf73ba5194b4ed4a395721e9376ae8c44d
j-rewerts <jaredrewerts@gmail.com>
2017-07-25 10:27:36 -0600

    Switched to using the new config file.

--- server/index.js
+++ server/index.js
@@ -3,26 +3,23 @@ const http = require('http');
 const Session = require('express-session');
 const MongoStore = require('connect-mongo')(Session);
 const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 const app = express();
 const router = require('./router.js');
 const socketHandler = require('./socket-handler.js');
 const taskRunner = require('./task-runner.js');
+const Config = require('./config.js');
 
 require('./util/map-to-json.js');
 require('./util/object-filter.js');
 require('./util/object-to-map.js');
 
-const port = 3000;
-const url = 'mongodb://localhost:27017/dev';
 const sess = {
-  secret: 'fasdkh7f4qjhadf6kashfr347ajpv',
+  secret: Config.Session.SECRET,
   resave: false,
   saveUninitialized: true,
-  store: new MongoStore({url}),
+  store: new MongoStore({url: Config.Database.URL}),
 }
-
-mongoose.Promise = global.Promise;
-mongoose.connect(url);
 
 // This grabs all unhandled Promise rejections and logs them. Otherwise, you get no stacktrace.
 // http://2ality.com/2016/04/unhandled-rejections.html
@@ -30,15 +27,16 @@ process.on('unhandledRejection', (reason) => {
     console.error(reason);
 });
 
-let session = Session(sess);
+mongoose.connect(Config.Database.URL);
 
+let session = Session(sess);
 router(session, app);
 
 const server = http.createServer(app);
 socketHandler(session, server);
 
-server.listen(port);
-console.log('Your server is running on port ' + port + '.');
+server.listen(Config.Web.PORT);
+console.log(`Your server is running on port ${Config.Web.PORT}.`);
 
 taskRunner();
 console.log('Tasks set up.');
