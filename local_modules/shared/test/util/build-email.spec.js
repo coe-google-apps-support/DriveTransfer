ed344ffc61581dbe68efa4ac105bf3adc464cdcf
j-rewerts <jaredrewerts@gmail.com>
2017-08-01 17:32:10 -0600

    Updated buildEmail tests.

--- local_modules/shared/test/util/build-email.spec.js
+++ local_modules/shared/test/util/build-email.spec.js
@@ -8,7 +8,6 @@ let eol = require('os').EOL;
 
 const INITIATOR = 'user1@email.com';
 const RECIPIENT = 'user2@email.com';
-const FOLDER_ID = '123456_folder';
 const TASK_ID = '123456_task';
 const TEST_EMAIL =
 `Content-Type: text/html; charset="UTF-8"${eol}\
@@ -19,7 +18,6 @@ from: ${INITIATOR}${eol}\
 subject: This is the subject${eol}\
 ${eol}\
 This is the body${eol}\
-Folder: ${FOLDER_ID}${eol}\
 Task: ${TASK_ID}${eol}\
 `;
 
@@ -28,12 +26,12 @@ describe('.buildEmail', () => {
   let emailWrongPath = path.join(__dirname, 'dne-email.txt');
 
   it('Successfully load an email', () => {
-    let promise = buildEmail(emailPath, RECIPIENT, FOLDER_ID, INITIATOR, TASK_ID);
+    let promise = buildEmail(emailPath, RECIPIENT, INITIATOR, TASK_ID);
     return assert.eventually.equal(promise, TEST_EMAIL);
   });
 
   it('Reject when email file dne', () => {
-    let promise = buildEmail(emailWrongPath, RECIPIENT, FOLDER_ID, INITIATOR, TASK_ID);
+    let promise = buildEmail(emailWrongPath, RECIPIENT, INITIATOR, TASK_ID);
     return assert.isRejected(promise);
   });
 });
