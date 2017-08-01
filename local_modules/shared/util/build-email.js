c7f7538d6b440a04cc5fd030908c4d27486bb78d
j-rewerts <jaredrewerts@gmail.com>
2017-08-01 16:57:00 -0600

    Updated the email building function.

--- local_modules/shared/util/build-email.js
+++ local_modules/shared/util/build-email.js
@@ -1,11 +1,10 @@
 const EJS = require('ejs');
 const readFile = require('./promisey-read-file.js').readFile;
 
-function buildRequestEmail(filePath, recipient, folderID, initiator, taskID) {
+function buildRequestEmail(filePath, recipient, initiator, taskID) {
   return readFile(filePath).then((result) => {
     return EJS.render(result, {
       recipient: recipient,
-      folderID: folderID,
       initiator: initiator,
       taskID: taskID
     });
