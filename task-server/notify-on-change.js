3cfd51cfd5e41047af1b64ba16b13ad5c2d37757
j-rewerts <jaredrewerts@gmail.com>
2017-08-01 17:01:36 -0600

    Added comments to the oplog notifier.

--- task-server/notify-on-change.js
+++ task-server/notify-on-change.js
@@ -2,7 +2,11 @@ const MongoOplog = require('mongo-oplog-filter');
 const Config = require('shared/config.js');
 
 /**
- * Watches the oplog for a single
+ * Watches the oplog for a single change. After resolving, the watcher is destroyed.
+ * @param {string} collection the collection name to watch for changes.
+ * @param {Function} filter A Function that receives a document from the oplog and
+ * returns true when it wants the Promsie to be resolved.
+ * @return {Promise} A Promise that is resolved when the filter returns true.
  */
 function onChange(collection, filter) {
   const oplog = MongoOplog(Config.Database.OP_LOG_URL);
