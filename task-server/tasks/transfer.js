72f5897d51307d79e8bf2e5ad3dcda0c9f64de0b
j-rewerts <jaredrewerts@gmail.com>
2017-08-01 17:03:36 -0600

    Added filtering for requests.
    This is the first part of TransferTasks being reimplemented.

--- task-server/tasks/transfer.js
+++ task-server/tasks/transfer.js
@@ -55,7 +55,14 @@ class Transfer extends Task {
       }
 
       this.requestTask = requestTask;
-    })
+      return TransferProvider.getRequestID(this.taskID);
+    }).then((id) => {
+      if (!id) {
+        throw new Error('Transfer failed fetching request task id');
+      }
+
+      this.requestID = id;
+    });
   }
 
   async run() {
@@ -74,8 +81,18 @@ class Transfer extends Task {
     if (this.transferState === TaskSubStates.SENDING_EMAIL) {
       console.log(TaskSubStates.SENDING_EMAIL);
       TaskProvider.run(this.requestTask);
-      return onChange('transfer_request_task', this.filterTransferRequest).then((doc) => {
-        console.log('Looks like the request was updated.');
+      return onChange('transfer_request_tasks', this.filterTransferRequest).then((doc) => {
+        const action = doc.o.$set.status;
+        if (action === 'rejected') {
+          // TODO reject task. Set to quit. Delete ALL associated tasks
+          throw new Error('IMPLEMENT REJECTED HANDLING');
+        }
+        else if (action === 'accepted') {
+          this.transferState = TaskSubStates.CREATING_FILTER
+        }
+        else {
+          throw new Error(`transfer_request_task must have status rejected or accepted, not ${action}`);
+        }
       });
     }
     else if (this.transferState === TaskSubStates.WAITING_FOR_CREDENTIALS) {
@@ -83,6 +100,9 @@ class Transfer extends Task {
     }
     else if (this.transferState === TaskSubStates.CREATING_FILTER) {
       console.log(TaskSubStates.CREATING_FILTER);
+      return new Promise((resolve, reject) => {
+
+      });
     }
     else if (this.transferState === TaskSubStates.LISTING) {
       console.log(TaskSubStates.SENDING_EMAIL);
@@ -100,8 +120,19 @@ class Transfer extends Task {
   }
 
   filterTransferRequest(doc) {
-    console.log('Filtering');
-    console.log(doc);
+    console.log(this.requestID)
+    console.log(doc.o2._id);
+
+    if (doc.op === 'u' &&
+      doc.o2 && doc.o2._id && doc.o2._id.toString() === this.requestID.toString() &&
+      doc.o && doc.o.$set && doc.o.$set.status) {
+
+      console.log('Filtering');
+      console.log(doc);
+      return true;
+    }
+
+    return false;
   }
 
 }
