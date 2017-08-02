22947c70452789f96f8cf605e9290d9495c57a40
j-rewerts <jaredrewerts@gmail.com>
2017-08-02 15:24:21 -0600

    The transfer task now actually transfers.

--- task-server/tasks/transfer.js
+++ task-server/tasks/transfer.js
@@ -3,6 +3,7 @@ const UserProvider = require('shared/providers/user-provider.js');
 const TransferProvider = require('shared/providers/transfer-provider.js');
 const TaskProvider = require('shared/providers/task-provider.js');
 const FilterProvider = require('shared/providers/transfer-filter-provider.js')
+const ListProvider = require('shared/providers/list-provider.js')
 const Config = require('shared/config.js');
 const exponentialBackoff = require('shared/util/exponential-backoff.js');
 const TaskStates = require('shared/task-states.js');
@@ -94,22 +95,23 @@ class Transfer extends Task {
       return onChange('transfer_request_tasks', this.filterTransferRequest).then((doc) => {
         const action = doc.o.$set.status;
         const userID = doc.o.$set['recipient.user'];
-        if (action === 'rejected') {
-          // TODO reject task. Set to quit. Delete ALL associated tasks
-          throw new Error('IMPLEMENT REJECTED HANDLING');
-        }
-        else if (action === 'accepted') {
+
+        if (action === 'accepted') {
           this.transferState = TaskSubStates.CREATING_FILTER;
           // TODO Use TransferProvider for the filter creation?
           return FilterProvider.create(this.taskID, userID);
         }
         else {
-          throw new Error(`transfer_request_task must have status rejected or accepted, not ${action}`);
+          throw new Error(`transfer_request_task was rejected.`);
         }
       }).then((filterTask) => {
         this.filterID = filterTask._id;
         this.filterTask = filterTask.task;
         return TransferProvider.setFilterTask(this.taskID, this.filterTask);
+      }).then(() => {
+        return TransferProvider.getRecipientDrive(this.taskID).then((drive) => {
+          this.recipientDrive = drive;
+        });
       });
     }
     else if (this.transferState === TaskSubStates.CREATING_FILTER) {
@@ -124,19 +126,51 @@ class Transfer extends Task {
       TaskProvider.run(this.listTask);
       return onChange('tasks', this.filterListRequest).then((doc) => {
         this.transferState = TaskSubStates.TRANSFERRING;
+        this.listValues = ListProvider.getResultCursor(this.listTask);
       });
-
     }
     else if (this.transferState === TaskSubStates.TRANSFERRING) {
       console.log(TaskSubStates.TRANSFERRING);
-      return new Promise((resolve, reject) => {});
+
+      let fileID;
+      let doneError = new Error('Finished iterating list.');
+      return this.listValues.next().then((file) => {
+        if (!file) {
+          this.transferState = TaskSubStates.FINISHED;
+          throw doneError;
+        }
+
+        fileID = file.id;
+        return exponentialBackoff(
+          this.transfer.bind(this, fileID, this.newOwnerEmail),
+          Config.ExpoBackoff.MAX_TRIES,
+          Config.ExpoBackoff.NAPTIME,
+          this.transferPredicate
+        );
+      }).then((transferResult) => {
+        return TransferProvider.addResult(this.taskID, fileID);
+      }).then(() => {
+        return exponentialBackoff(
+          this.removeFileFromRoot.bind(this, fileID),
+          Config.ExpoBackoff.MAX_TRIES,
+          Config.ExpoBackoff.NAPTIME,
+          this.transferPredicate
+        );
+      }).catch((err) => {
+        if (err !== doneError) {
+          throw err;
+        }
+      });
     }
     else if (this.transferState === TaskSubStates.FINISHED) {
       console.log(TaskSubStates.FINISHED);
+      this.interrupt();
+      return TaskProvider.finish(this.taskID);
     }
     else {
-      console.log(`Weird TransferState: ${this.transferState}`);
+      console.log(`Unknown TransferState: ${this.transferState}`);
       this.interrupt();
+      return TaskProvider.fail(this.taskID);
     }
   }
 
@@ -173,6 +207,65 @@ class Transfer extends Task {
     }
 
     return false;
+  }
+
+  transferPredicate(error) {
+    if (!error.errors || error.errors[0]) {
+      console.log('non-api error');
+      return false;
+    }
+
+    let firstError = error.errors[0];
+    if ((error.code === 403 && firstError.reason === 'userRateLimitExceeded') ||
+      (error.code === 403 && firstError.reason === 'rateLimitExceeded') ||
+      (error.code === 429 && firstError.reason === 'rateLimitExceeded') ||
+      (error.code === 500 && firstError.reason === 'backendError')) {
+      console.log('Retrying');
+      return true;
+    }
+
+    console.log('Don\'t retry');
+    return false;
+  }
+
+  transfer(fileID, newOwnerEmail) {
+    return new Promise((resolve, reject) => {
+      this.drive.permissions.create({
+        fileId: fileID,
+        transferOwnership: true,
+        emailMessage: Config.Tasks.Transfer.EMAIL_MESSAGE,
+        resource: {
+          emailAddress: newOwnerEmail,
+          role: 'owner',
+          type: 'user',
+        },
+      }, function(err, response) {
+        if (err != null) {
+          reject(err);
+          return;
+        }
+
+        resolve(response);
+      });
+    });
+  }
+
+  removeFileFromRoot(fileID) {
+    console.log(`removing ${fileID}`);
+    return new Promise((resolve, reject) => {
+      this.recipientDrive.files.update({
+        fileId: fileID,
+        removeParents: 'root'
+      },
+        function(err, response) {
+          if (err != null) {
+            reject(err);
+            return;
+          }
+
+          resolve();
+      });
+    });
   }
 
 }
