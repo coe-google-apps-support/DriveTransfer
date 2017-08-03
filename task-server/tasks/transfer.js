2d61c9bc658980ed3a71e2372cf2604ae2c2d68f
j-rewerts <jaredrewerts@gmail.com>
2017-08-02 18:48:57 -0600

    Removed TransferFilterProvider dependency.

--- task-server/tasks/transfer.js
+++ task-server/tasks/transfer.js
@@ -1,8 +1,6 @@
 const Task = require('./task.js');
-const UserProvider = require('shared/providers/user-provider.js');
 const TransferProvider = require('shared/providers/transfer-provider.js');
 const TaskProvider = require('shared/providers/task-provider.js');
-const FilterProvider = require('shared/providers/transfer-filter-provider.js')
 const ListProvider = require('shared/providers/list-provider.js')
 const Config = require('shared/config.js');
 const exponentialBackoff = require('shared/util/exponential-backoff.js');
@@ -21,10 +19,7 @@ class Transfer extends Task {
   constructor(taskID) {
     super(taskID);
     this.taskID = taskID;
-    this.whenDone = new Promise((resolve, reject) => {
-      this._whenDoneResolve = resolve;
-      this._whenDoneReject = reject;
-    });
+
     this.transferState = TaskSubStates.SENDING_EMAIL;
     this.filterTransferRequest = this.filterTransferRequest.bind(this);
     this.filterFilterRequest = this.filterFilterRequest.bind(this);
@@ -59,13 +54,6 @@ class Transfer extends Task {
       }
 
       this.requestTask = requestTask;
-      return TransferProvider.getRequestID(this.taskID);
-    }).then((id) => {
-      if (!id) {
-        throw new Error('Transfer failed fetching request task id');
-      }
-
-      this.requestID = id;
       return TransferProvider.getListTask(this.taskID);
     }).then((listTask) => {
       if (!listTask) {
@@ -91,24 +79,13 @@ class Transfer extends Task {
   doWork() {
     if (this.transferState === TaskSubStates.SENDING_EMAIL) {
       console.log(TaskSubStates.SENDING_EMAIL);
-      TaskProvider.run(this.requestTask);
-      return onChange('transfer_request_tasks', this.filterTransferRequest).then((doc) => {
-        const action = doc.o.$set.status;
-        const userID = doc.o.$set['recipient.user'];
 
-        if (action === 'accepted') {
-          this.transferState = TaskSubStates.CREATING_FILTER;
-          // TODO Use TransferProvider for the filter creation?
-          return FilterProvider.create(this.taskID, userID);
-        }
-        else {
-          throw new Error(`transfer_request_task was rejected.`);
-        }
+      TaskProvider.run(this.requestTask);
+      return onChange('tasks', this.filterTransferRequest).then((action) => {
+        this.transferState = TaskSubStates.CREATING_FILTER;
+        return TransferProvider.createFilter(this.taskID);
       }).then((filterTask) => {
-        this.filterID = filterTask._id;
         this.filterTask = filterTask.task;
-        return TransferProvider.setFilterTask(this.taskID, this.filterTask);
-      }).then(() => {
         return TransferProvider.getRecipientDrive(this.taskID).then((drive) => {
           this.recipientDrive = drive;
         });
@@ -116,13 +93,15 @@ class Transfer extends Task {
     }
     else if (this.transferState === TaskSubStates.CREATING_FILTER) {
       console.log(TaskSubStates.CREATING_FILTER);
+
       TaskProvider.run(this.filterTask);
-      return onChange('transfer_filter_tasks', this.filterFilterRequest).then((doc) => {
+      return onChange('tasks', this.filterFilterRequest).then((doc) => {
         this.transferState = TaskSubStates.LISTING;
       });
     }
     else if (this.transferState === TaskSubStates.LISTING) {
       console.log(TaskSubStates.LISTING);
+
       TaskProvider.run(this.listTask);
       return onChange('tasks', this.filterListRequest).then((doc) => {
         this.transferState = TaskSubStates.TRANSFERRING;
@@ -164,6 +143,7 @@ class Transfer extends Task {
     }
     else if (this.transferState === TaskSubStates.FINISHED) {
       console.log(TaskSubStates.FINISHED);
+
       this.interrupt();
       return TaskProvider.finish(this.taskID);
     }
@@ -175,12 +155,11 @@ class Transfer extends Task {
   }
 
   filterTransferRequest(doc) {
+    console.log(doc)
     if (doc.op === 'u' &&
-      doc.o2 && doc.o2._id && doc.o2._id.toString() === this.requestID.toString() &&
-      doc.o && doc.o.$set &&
-      doc.o.$set.status &&
-      doc.o.$set['recipient.user']) {
-
+      doc.o2 && doc.o2._id && doc.o2._id.toString() === this.requestTask.toString() &&
+      doc.o && doc.o.$set && doc.o.$set.state && doc.o.$set.state === TaskStates.FINISHED) {
+      console.log('success');
       return true;
     }
 
