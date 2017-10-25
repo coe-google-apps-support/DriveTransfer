e7903e9d08fcb74fbb7d47cabe907f29ae4fcf4e
j-rewerts <jaredrewerts@gmail.com>
2017-10-24 18:01:36 -0600

    Added cleaner db urls.

--- local_modules/shared/config.js
+++ local_modules/shared/config.js
@@ -1,7 +1,8 @@
 /// This file is used to provide configuration options to Drive Transfer's request and task servers.
-/// Currently, 2 values are required to be passed in via environment variable:
-/// Config.OAuth.CLIENT_ID and Config.OAuth.CLIENT_SECRET
-/// These can be gotten from the cloud console credentials section.
+/// Currently, 4 values are required to be passed in via environment variable:
+/// Config.OAuth.CLIENT_ID, Config.OAuth.CLIENT_SECRET, Config.Database.USER and Config.Database.PASSWORD
+/// The first 2 can be gotten from the cloud console credentials section.
+/// The latter 2 are for authenticating to the MongoDB database.
 
 const Config = {};
 
@@ -38,11 +39,15 @@ Config.Web = {};
 Config.Web.PORT = process.env.DT_WEB_PORT || 3000;
 
 Config.Database = {};
+let db = Config.Database;
 Config.Database.NAME = process.env.DT_DATABASE || 'dev';
+Config.Database.REPL_NAME = process.env.DT_REPL_SET || 'test';
 Config.Database.PORT = process.env.MONGO_PORT_27017_TCP_PORT || 27017;
 Config.Database.HOST = process.env.MONGO_PORT_27017_TCP_ADDR  || 'localhost';
-Config.Database.URL = 'mongodb://' + Config.Database.HOST + ':' + Config.Database.PORT + '/' + Config.Database.NAME;
-Config.Database.OP_LOG_URL = 'mongodb://' + Config.Database.HOST + ':' + Config.Database.PORT + '/local';
+Config.Database.USER = process.env.MONGO_INITDB_ROOT_USERNAME;
+Config.Database.PASSWORD = process.env.MONGO_INITDB_ROOT_PASSWORD;
+Config.Database.URL = `mongodb://${db.USER}:${db.PASSWORD}@${db.HOST}:${db.PORT}/${db.NAME}?authSource=admin&replicaSet=${db.REPL_NAME}`;
+Config.Database.OP_LOG_URL = `mongodb://${db.USER}:${db.PASSWORD}@${db.HOST}:${db.PORT}/local?authSource=admin&replicaSet=${db.REPL_NAME}`;
 
 Config.Session = {};
 Config.Session.SECRET = process.env.DT_SESSION_SECRET || 'default-secret';
@@ -55,6 +60,12 @@ if (!Config.OAuth.CLIENT_ID) {
 }
 if (!Config.OAuth.CLIENT_SECRET) {
   throw new Error('Please set the DT_CLIENT_SECRET environment variable.');
+}
+if (!Config.Database.USER) {
+  throw new Error('Please set the MONGO_INITDB_ROOT_USERNAME environment variable.');
+}
+if (!Config.Database.PASSWORD) {
+  throw new Error('Please set the MONGO_INITDB_ROOT_PASSWORD environment variable.');
 }
 
 module.exports = Config;
