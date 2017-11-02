7b524f690e7570119248e746a2707697c6899e8a
Jared Rewerts <jaredrewerts@gmail.com>
2017-11-02 16:22:23 -0600

    Push some config info to client.

--- request-server/routers/main.js
+++ request-server/routers/main.js
@@ -1,6 +1,11 @@
 const path = require('path');
 const UserProvider = require('shared/providers/user-provider.js');
+const Config = require('shared/config.js');
 
 exports.view = function(req, res, next) {
-  res.render('index');
+  res.render('index', {
+    constants: {
+      DT_SOCKET_URL: Config.Web.WEBSOCKET_URL
+    }
+  });
 }
